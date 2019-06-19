f0655cf2907046d79d4d75b0fae3642082be752d
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-06-19 23:13:41 +0530

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,22 @@
+n,q=map(int,input().split())
+y=[]
+b=[]
+d=0
+c=0
+a=[int(n) for n in input().split() ]
+for i in range(0,q):
+    u,v=map(int,input().split())
+    for j in range(u-1 ,v):
+        b.append(a[j])
+#    x=sorted(b)
+    y.append(min(b))
+    del b[:]
+
+for z in range(0,len(y)):
+    print(y[z])
+        
+        
+        
+        
+        
+        
